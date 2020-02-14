8b2a5335f522a4693bbb95bf8d58525fb8b82642
nfagan <nick@fagan.org>
2020-02-14 17:31:25 -0500

    remove ctor util

--- src/mt/utility.hpp
+++ src/mt/utility.hpp
@@ -11,7 +11,3 @@
 #else
 #define MSVC_MISSING_NOEXCEPT noexcept
 #endif
-
-#define MT_DELETE_COPY_CTOR_AND_ASSIGNMENT(class_name) \
-  class_name(const class_name& other) = delete; \
-  class_name& operator=(const class_name& other) = delete;
