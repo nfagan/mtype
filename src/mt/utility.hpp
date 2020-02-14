aebfdecfc541c1793bdd5e200fd8e9ad8eac446d
nfagan <nick@fagan.org>
2020-02-14 17:29:06 -0500

    add addtl utils

--- src/mt/utility.hpp
+++ src/mt/utility.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 #ifdef _MSC_VER
+#define MT_IS_MSVC (1)
+#else
+#define MT_IS_MSVC (0)
+#endif
+
+#if MT_IS_MSVC
 #define MSVC_MISSING_NOEXCEPT
 #else
 #define MSVC_MISSING_NOEXCEPT noexcept
 #endif
+
+#define MT_DELETE_COPY_CTOR_AND_ASSIGNMENT(class_name) \
+  class_name(const class_name& other) = delete; \
+  class_name& operator=(const class_name& other) = delete;
